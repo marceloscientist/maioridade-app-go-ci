defee0b33ff8617454856f89b08b42a8143a52b7
Marcelo Carvalho <marcelo.santana@edu.sc.senai.br>
2024-12-30 16:54:16 -0300

    ci: some fixings plus sonar cloud

--- main.go
+++ main.go
@@ -1,17 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	idade, err := obterIdade()
+
+	if err := carregarEnv(""); err != nil {
+		fmt.Println("Erro ao carregar as variáveis de ambiente:", err)
+		return
+	}
+	run(os.Stdin, os.Stdout)
+}
+
+func run(input io.Reader, output io.Writer) {
+	idade, err := obterIdade(input)
 	if err != nil {
-		fmt.Println("Erro:", err)
+		fmt.Fprintln(output, "Erro:", err)
 		return
 	}
 
 	if ehMaiorDeIdade(idade) {
-		fmt.Println("Você é maior de idade.")
+		fmt.Fprintln(output, "Você é maior de idade.")
 	} else {
-		fmt.Println("Você é menor de idade.")
+		fmt.Fprintln(output, "Você é menor de idade.")
 	}
+}
+
+func obterIdade(input io.Reader) (int, error) {
+	fmt.Println("Qual a sua idade?")
+	scanner := bufio.NewScanner(input)
+	scanner.Scan()
+	idadeStr := scanner.Text()
+
+	idade, err := strconv.Atoi(idadeStr)
+	if err != nil {
+		return 0, fmt.Errorf("digite um número válido")
+	}
+	return idade, nil
+}
+
+func ehMaiorDeIdade(idade int) bool {
+	return idade >= 18
+}
+
+func carregarEnv(path ...string) error {
+	err := godotenv.Load(path...)
+	if err == nil {
+		fmt.Println("Variáveis carregadas do .env:")
+		fmt.Println("SONAR_TOKEN:", os.Getenv("SONAR_TOKEN"))
+		fmt.Println("SONAR_HOST_URL:", os.Getenv("SONAR_HOST_URL"))
+	}
+	return err
 }
